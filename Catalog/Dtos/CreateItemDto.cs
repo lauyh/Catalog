49cdd5e132607d56446f33b975a2f40ea8357eba
lauyh <lauyh87@gmail.com>
2021-08-09 02:44:44 +0800

    Add DTO

---
+++ Catalog/Dtos/CreateItemDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Catalog.Dtos
+{
+    public record CreateItemDto
+    {
+        [Required]
+        public string Name { get; set; }
+        
+        [Required]
+        public decimal Price { get; init; }
+    }
+}
