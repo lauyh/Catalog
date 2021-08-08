f099113e8796966b2aeefef7aaeac8dea69e7c3d
lauyh <lauyh87@gmail.com>
2021-08-09 02:46:08 +0800

    add POST endpoint to add new item

--- Catalog/Controllers/ItemController.cs
+++ Catalog/Controllers/ItemController.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Catalog.Dtos;
 
 namespace Catalog.Controllers
 {
@@ -12,31 +13,48 @@ namespace Catalog.Controllers
     [Route("items")]
     public class ItemController : ControllerBase
     {
-        private readonly InMemItemsRepository repository;
+        private readonly IItemsRepository repository;
 
-        public ItemController()
+        public ItemController(IItemsRepository repository)
         {
-            repository = new InMemItemsRepository();
+            this.repository = repository;
 
         }
 
         // GET /items
         [HttpGet]
-        public IEnumerable<Item> GetItems()
+        public IEnumerable<ItemDto> GetItems()
         {
-            var items = repository.GetItems();
+            var items = repository.GetItems().Select(item => item.AsDto());
             return items;
         }
-
+        
+        // GET /items/{id}
         [HttpGet("{id}")]
-        public ActionResult<Item> GetItem(Guid id)
+        public ActionResult<ItemDto> GetItem(Guid id)
         {
             var item = repository.GetItem(id);
             if(item is null)
             {
                 return NotFound();
             }
-            return item;
+            return item.AsDto();
+        }
+        
+        // POST /items
+        [HttpPost]
+        public ActionResult<ItemDto> CreateItem(CreateItemDto itemDto)
+        {
+            Item item = new()
+            {
+                Id = Guid.NewGuid(),
+                Name = itemDto.Name,
+                Price = itemDto.Price,
+                CreatedDate = DateTime.UtcNow
+            };
+
+            repository.CreateItem(item);
+            return CreatedAtAction(nameof(GetItem), new { id = item.Id }, item.AsDto());
         }
     }
 }
